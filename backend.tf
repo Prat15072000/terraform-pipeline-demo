7efdd7c57efc7dfd6c9e30b40601e2bf37cd5ae8
Pratyay <pratsimple2000@gmail.com>
2025-05-26 02:49:05 +0530

    demo-architecture-setup-files

---
+++ backend.tf
@@ -0,0 +1,8 @@
+terraform {
+    backend "azurerm" {
+      resource_group_name = "mgRG"
+      storage_account_name = "statefileforprat"
+      container_name = "statefilecontainer"
+      key = "terraform.tfstate"
+    }
+}
---
+++ main.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "4.19.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  # Configuration options
+  subscription_id = "7e185f1c-e860-425a-adf9-922e298f43dc"
+  features {
+    resource_group {
+      prevent_deletion_if_contains_resources = false
+    }
+  }
+}
