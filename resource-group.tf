c86d533f5adaa05f53da9b51fadc31523f1ad333
Pratyay <pratsimple2000@gmail.com>
2025-05-28 15:55:03 +0530

    terraform-deployment-day4-statefiles-demo

--- resource-group.tf
+++ resource-group.tf
@@ -1,5 +1,5 @@
-resource "azurerm_resource_group" "march-rg" {
-  name     = var.resource_group_name
-  location = "West US"
-  tags = var.tags
-}
+# resource "azurerm_resource_group" "march-rg" {
+#   name     = var.resource_group_name
+#   location = "West US"
+#   tags = var.tags
+# }
