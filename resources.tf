f7906d14f25cb9a40cea5cf54ca2ee66c81072af
Pratyay <pratsimple2000@gmail.com>
2025-06-02 03:05:41 +0530

    edit

--- resources.tf
+++ resources.tf
@@ -1,4 +1,4 @@
-# resource "azurerm_resource_group" "tf-rg" {
-#   name = "tf-rggg"
-#   location = "East US"
-# }
+resource "azurerm_resource_group" "tf-rg" {
+  name = "tf-rggg"
+  location = "East US"
+}
